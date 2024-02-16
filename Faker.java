20ee370809a1c393bfd686aca6058855b506a28f
Aviral Jain <48090218+aviralJain101@users.noreply.github.com>
2024-02-17 01:33:52 +0530

    Update Faker.java

--- Faker.java
+++ Faker.java
@@ -3,56 +3,37 @@ package com.itscreening.tests.faker;
 import java.util.Random;
 
 public class Faker {
-
     private static final Random random = new Random();
-
-    /**
-     * Replaces each question mark character (?) with any random lower a-z letter.
-     *
-     * @param letterString the string containing question mark characters to replace
-     * @return a string with question mark characters replaced with random lower a-z letters
-     */
     public String letterify(String letterString) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder letter = new StringBuilder();
         for (int i = 0; i < letterString.length(); i++) {
-            char c = letterString.charAt(i);
-            if (c == '?') {
-                result.append((char) ('a' + random.nextInt(26))); // Generating random lower a-z letter
+            char element = letterString.charAt(i);
+            if (element == '?') {
+                letter.append((char) ('a' + random.nextInt(26))); // Generating random lower a-z letter
             } else {
-                result.append(c);
+                letter.append(element);
             }
         }
-        return result.toString();
+        return letter.toString();
     }
 
-    /**
-     * Replaces each hash character (#) with any random 0-9 number.
-     *
-     * @param numberString the string containing hash characters to replace
-     * @return a string with hash characters replaced with random 0-9 numbers
-     */
     public String numerify(String numberString) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder letter = new StringBuilder();
         for (int i = 0; i < numberString.length(); i++) {
-            char c = numberString.charAt(i);
-            if (c == '#') {
-                result.append(random.nextInt(10)); // Generating random 0-9 number
+            char element = numberString.charAt(i);
+            if (element == '#') {
+                letter.append(random.nextInt(10)); // Generating random 0-9 number
             } else {
-                result.append(c);
+                letter.append(element);
             }
         }
-        return result.toString();
+        return letter.toString();
+
     }
 
-    /**
-     * Replaces each question mark character (?) with any random lower a-z letter
-     * and each hash character (#) with any random 0-9 number.
-     *
-     * @param string the string containing both question mark and hash characters to replace
-     * @return a string with question mark characters replaced with random lower a-z letters
-     * and hash characters replaced with random 0-9 numbers
-     */
     public String bothify(String string) {
         return numerify(letterify(string));
+
     }
+
 }
